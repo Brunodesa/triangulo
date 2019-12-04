67689b679649ee46e0f3a87487c7373fcbc2e7a6
Brunodesa <49911022+Brunodesa@users.noreply.github.com>
2019-12-04 12:07:56 +0000

    Add files via upload

---
+++ DoisPontos-master/DoisPontos/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace DoisPontos
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Triangulo T = new Triangulo();
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            T.P1.X = Convert.ToDouble(x1.Text);
+            T.P1.Y = Convert.ToDouble(y1.Text);
+            T.P1.Z = Convert.ToDouble(z1.Text);
+            T.P2.X = Convert.ToDouble(x2.Text);
+            T.P2.Y = Convert.ToDouble(y2.Text);
+            T.P2.Z = Convert.ToDouble(z2.Text);
+            T.P3.X = Convert.ToDouble(x3.Text);
+            T.P3.Y = Convert.ToDouble(y3.Text);
+            T.P3.Z = Convert.ToDouble(z3.Text);
+            if (area.Checked) label1.Text = T.Area.ToString();
+            else if (perimetro.Checked) label1.Text = T.Perimetro.ToString();
+        }
+    }
+}
---
+++ DoisPontos-master/DoisPontos/Ponto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace DoisPontos
+{
+    class Ponto
+    {
+        private double _X, _Y, _Z;
+        public double X { set => _X = value; get => _X; }
+        public double Y { set => _Y = value; get => _Y; }
+        public double Z { set => _Z = value; get => _Z; }
+        public double Distancia(Ponto P) =>
+        Math.Sqrt(Math.Pow(P.X - _X, 2) + Math.Pow(P.Y - _Y, 2) + Math.Pow(P.Z - _Z, 2));
+    }
+}
